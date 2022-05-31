12529a6391506fde20a8100ee1508e3ad83668f2
Bengsiswanto <bengsiswantoh@gmail.com>
2022-05-31 14:53:50 +0700

    chore(link and meta): add link and meta tag use proper nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Head from "next/head";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,5 @@
-import React from "react";
-
-import Container from "@material-ui/core/Container";
-
-import Header from "../components/Header";
-
-import { sections } from "../data/sections";
+import Container from "@mui/material/Container";
 
 export default function Home() {
-  return (
-    <React.Fragment>
-      <Container maxWidth="lg">
-        <Header title="Bengsiswanto Hendrawan" sections={sections} />
-      </Container>
-    </React.Fragment>
-  );
+  return <Container></Container>;
 }
