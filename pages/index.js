300f5f1d412b9780bc84a6e46c40e6321d292b40
Bengsiswanto <bengsiswantoh@gmail.com>
2022-07-14 09:17:20 +0700

    add profile image

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,51 @@
-import { Container, Box, Heading } from "@chakra-ui/react";
+import { Container, Box, Heading, useColorModeValue } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        p={3}
+        mb={6}
+        textAlign="center"
+        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+        css={{ backdropFilter: "blur(10px)" }}
+      >
         Hello
       </Box>
 
-      <Box display="flex">
+      <Box display={{ md: "flex" }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             Bengsiswanto Hendrawan
           </Heading>
+          <p>Developer</p>
+        </Box>
+
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            {/* <ProfileImage
+              src="/images/"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            /> */}
+          </Box>
         </Box>
       </Box>
     </Container>
