474b047bd080010a48e15201ca57af0112160b47
Bengsiswanto <bengsiswantoh@gmail.com>
2022-07-14 08:59:42 +0700

    change font

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Layout from "../components/layouts";
+import Fonts from "../components/fonts";
 import theme from "../theme";
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
+
       <Layout router={router}>
         <Component {...pageProps} />
       </Layout>
