176b4ee27baf3802e61dcc848dd5a1bd65a75dd7
bengsiswanto <bengsiswantoh@gmail.com>
2022-07-14 23:32:54 +0700

    add works

---
+++ pages/works.js
@@ -0,0 +1,40 @@
+import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
+
+import Section from "../components/section";
+import { WorkGridItem } from "../components/grid-item";
+
+const Works = () => (
+  <Container>
+    <Heading as="h3" fontSize={20} mb={4}>
+      Works
+    </Heading>
+
+    <SimpleGrid columns={[1, 1, 2]} gap={6}>
+      <Section>
+        <WorkGridItem id="work1" title="Work 1">
+          Work 1
+        </WorkGridItem>
+      </Section>
+
+      <Section>
+        <WorkGridItem id="work2" title="Work 2">
+          Work 2
+        </WorkGridItem>
+      </Section>
+
+      <Section>
+        <WorkGridItem id="work3" title="Work 3">
+          Work 3
+        </WorkGridItem>
+      </Section>
+
+      <Section>
+        <WorkGridItem id="work4" title="Work 4">
+          Work 4
+        </WorkGridItem>
+      </Section>
+    </SimpleGrid>
+  </Container>
+);
+
+export default Works;
