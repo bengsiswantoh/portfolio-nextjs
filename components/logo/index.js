2202044b62c95046e4f6cb4ee631e14374cfc975
bengsiswanto <bengsiswantoh@gmail.com>
2022-07-15 22:13:24 +0700

    add image template

--- components/logo/index.js
+++ components/logo/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+// import Image from "next/image";
 import { Text, useColorModeValue } from "@chakra-ui/react";
 import styled from "@emotion/styled";
 
@@ -19,10 +20,13 @@ const LogoBox = styled.span`
 `;
 
 const Logo = () => {
+  // const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+
   return (
     <Link href="/" scroll={false}>
       <a>
         <LogoBox>
+          {/* <Image src={footPrintImg} width={20} height={20} alt="logo" /> */}
           <Text
             color={useColorModeValue("gray.800", "whiteAlpha.900")}
             fontFamily='M PLUS Rounded 1c", sans-serif'
