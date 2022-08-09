5a64859b8d3754162b2650e6598bb576c85286f6
Bengsiswanto <bengsiswantoh@gmail.com>
2022-08-09 08:46:41 +0700

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require("path");
 
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "public/game"),
   },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["bundle.js.LICENSE.txt"],
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets"),
+          to: path.resolve(__dirname, "public/assets"),
+        },
+      ],
+    }),
+  ],
 };
