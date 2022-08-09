438c192a237b4e39e9205d527b3416f251718c23
Bengsiswanto <bengsiswantoh@gmail.com>
2022-08-09 08:46:25 +0700

    add dev and change script src

--- pages/game.js
+++ pages/game.js
@@ -3,7 +3,8 @@ import Script from "next/script";
 export default function Game() {
   return (
     <>
-      <Script src="bundle.js" strategy="lazyOnload"></Script>
+      <Script src="game/bundle.js" strategy="lazyOnload"></Script>
+      <div id="game"></div>
     </>
   );
 }
